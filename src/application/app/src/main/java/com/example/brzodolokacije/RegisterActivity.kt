d6ae4ff9814439f3870b7142b2c71f41946dd84c
kiiviiii <ivana.dragovic07@gmail.com>
2022-10-28 23:14:21 +0200

    Skriven action bar

--- src/application/app/src/main/java/com/example/brzodolokacije/RegisterActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/RegisterActivity.kt
@@ -7,5 +7,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        supportActionBar?.hide()
     }
 }
