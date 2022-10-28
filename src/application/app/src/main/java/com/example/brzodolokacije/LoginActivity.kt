eca35d311a9a37eb4a700f32636a860a610f5f48
KatVir <94877518+KatVir@users.noreply.github.com>
2022-10-28 23:41:35 +0200

    login pozadina finished (hopefully)

---
+++ src/application/app/src/main/java/com/example/brzodolokacije/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.brzodolokacije
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
