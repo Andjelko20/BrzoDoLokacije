6ee191a1ffd21e2174c511a49b88f39ecd36a3ca
KatVir <94877518+KatVir@users.noreply.github.com>
2022-10-29 05:15:07 +0200

    #3 progress

--- src/application/app/src/main/java/com/example/brzodolokacije/LoginActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/LoginActivity.kt
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin=findViewById<Button>(R.id.loginBtn)
+        btnLogin.setOnClickListener{
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
     }
 }
