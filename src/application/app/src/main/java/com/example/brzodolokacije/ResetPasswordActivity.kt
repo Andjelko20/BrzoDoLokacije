9bed09cdad6b04a2a956c2cf1ce079b078d00cac
KatVir <94877518+KatVir@users.noreply.github.com>
2022-11-03 20:09:05 +0100

    #10 mali deo funkcionalnosti

--- src/application/app/src/main/java/com/example/brzodolokacije/ResetPasswordActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/ResetPasswordActivity.kt
@@ -3,18 +3,30 @@ package com.example.brzodolokacije
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_register.*
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_resetpassword.*
-import kotlinx.android.synthetic.main.activity_resetpassword.backToLogin
 
 class ResetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resetpassword)
 
-        backToLogin.setOnClickListener{
+        resetToLogin.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        resetPasswordBtn.setOnClickListener{
+            var email=findViewById<EditText>(R.id.registeredEmail).text.toString().trim()
+
+            if(email.isEmpty()){
+                registeredEmail.error = "Username or Email required"
+                registeredEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            Toast.makeText(this,"reset password clicked",Toast.LENGTH_SHORT).show()
         }
     }
 }
