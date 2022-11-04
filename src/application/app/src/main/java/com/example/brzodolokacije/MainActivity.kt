6d5eb8332c02b53603ecb068491d842e65412695
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-04 19:11:54 +0100

    #14 #15 Zavrsena implementacija
    Dodat je listener za bottom nav bar, u okviru kog se poziva funkcija replaceFragment

--- src/application/app/src/main/java/com/example/brzodolokacije/MainActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.brzodolokacije.databinding.ActivityMainBinding
+import com.example.brzodolokacije.fragments.ExploreFragment
+import com.example.brzodolokacije.fragments.HomeFragment
+import com.example.brzodolokacije.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.explore -> replaceFragment(ExploreFragment())
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else -> {}
+            }
+            true
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
