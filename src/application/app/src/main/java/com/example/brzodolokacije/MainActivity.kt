81dedfce97066855859ac14a1834993e630e894f
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-04 16:22:47 +0100

    #14 #15 Dodata funkcija u Main Activity koja vrsi switch-ovanje fragmenata

--- src/application/app/src/main/java/com/example/brzodolokacije/MainActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.brzodolokacije
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.brzodolokacije.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
