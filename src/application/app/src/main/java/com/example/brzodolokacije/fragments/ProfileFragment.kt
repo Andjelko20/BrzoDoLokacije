18e932443f3ba39f57b345005c5366032724531b
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-06 18:27:31 +0100

    #21 Konacno uspesan pristup dugmetu iz fragmenta

--- src/application/app/src/main/java/com/example/brzodolokacije/fragments/ProfileFragment.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/fragments/ProfileFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import com.example.brzodolokacije.API.Api
 import com.example.brzodolokacije.Client.Client
@@ -50,6 +51,14 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.logoutButton)
+        button.setOnClickListener { view ->
+            Log.d("btnLogout", "Selected")
+        }
     }
 
     companion object {
