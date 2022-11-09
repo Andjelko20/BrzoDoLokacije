93ec5427e4e11b303c64e5ccb9519b536bd21744
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-09 21:53:02 +0100

    #25 Ispravljen typo

--- src/application/app/src/main/java/com/example/brzodolokacije/Models/UserProfile.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Models/UserProfile.kt
@@ -1,12 +1,12 @@
 package com.example.brzodolokacije.Models
 
 data class UserProfile(
-    var userID: Int,
+    val userID: Int,
     val username: String,
     val firstname: String,
     val lastname: String,
-    val desctiption: String,
-    val profilePictue: String,
+    val description: String,
+    val profilePicture: String,
     val numOfFollowers: Int,
     val numOfFollowing: Int,
     val totalNumOfLikes: Int
