548692e7a344cea3a34d4bbdc10d6cceff6b1832
KatVir <94877518+KatVir@users.noreply.github.com>
2022-11-11 17:55:51 +0100

    .

--- src/application/app/src/main/java/com/example/brzodolokacije/Posts/Photo.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Posts/Photo.kt
@@ -5,9 +5,9 @@ import java.util.Date
 data class Photo (
     val postID : String,
     val path : String,
-    val owner : String,
+    val owner : String, // ne mora
     val ownerID : String,
-    val dateTime : String,
+    val dateTime : String, //Long
     val location : String, //za sad je string, mozda se menja
     val caption : String,
     val numOfLikes : Int,
