cd991492d5c3c73b8c5593b32ed803712abe4929
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-13 01:50:47 +0100

    Ispravljen base url ponovo

--- src/application/app/src/main/java/com/example/brzodolokacije/Constants/Constants.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Constants/Constants.kt
@@ -3,7 +3,7 @@ package com.example.brzodolokacije.Constants
 import android.content.res.Resources
 
 object Constants {
-    const val BASE_URL = "http://softeng.pmf.kg.ac.rs:10051/api/"
+    const val BASE_URL = "http://10.0.2.2:7281/api/"
 
     var screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels
     var screenHeight= screenWidth*3/4
