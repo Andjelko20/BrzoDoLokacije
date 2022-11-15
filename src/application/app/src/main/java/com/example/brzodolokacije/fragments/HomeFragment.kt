341c34d11c7ce6eb93eb2cd614713bb375a59f61
KatVir <94877518+KatVir@users.noreply.github.com>
2022-11-15 01:01:48 +0100

    dodat progress bar u home fragmentu

--- src/application/app/src/main/java/com/example/brzodolokacije/fragments/HomeFragment.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/fragments/HomeFragment.kt
@@ -1,12 +1,12 @@
 package com.example.brzodolokacije.Fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.brzodolokacije.API.Api
@@ -58,7 +58,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        view.findViewById<ProgressBar>(R.id.progressBar).setVisibility(View.VISIBLE)
         val retrofit = Client(requireActivity()).buildService(Api::class.java)
         retrofit.getAllPosts().enqueue(object: Callback<DefaultResponse>
         {
@@ -70,6 +70,7 @@ class HomeFragment : Fragment() {
                     val typeToken = object : TypeToken<List<Photo>>() {}.type
                     val photosList = Gson().fromJson<List<Photo>>(listOfPhotosStr, typeToken)
 
+                    view.findViewById<ProgressBar>(R.id.progressBar).setVisibility(View.GONE)
 
                     homePostsRv.apply {
                         mylayoutManager = LinearLayoutManager(context) //activity
