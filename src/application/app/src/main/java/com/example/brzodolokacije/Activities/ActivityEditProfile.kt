6054f232423b27cff10e89b84f6d2b46c073817d
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-18 01:08:00 +0100

    #40 Postavljen click listener za back button kod editovanja profila

--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/ActivityEditProfile.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/ActivityEditProfile.kt
@@ -1,8 +1,11 @@
 package com.example.brzodolokacije.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.brzodolokacije.R
+import kotlinx.android.synthetic.main.activity_editprofile.*
 
 class ActivityEditProfile : AppCompatActivity() {
 
@@ -10,5 +13,12 @@ class ActivityEditProfile : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editprofile)
+
+        val buttonBack = findViewById<Button>(R.id.backButton)
+        backButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("backPressed", "returnToProfile");
+            startActivity(intent)
+        }
     }
 }
--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/MainActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/MainActivity.kt
@@ -2,18 +2,20 @@ package com.example.brzodolokacije.Activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.brzodolokacije.API.Api
 import com.example.brzodolokacije.Client.Client
-import com.example.brzodolokacije.Fragments.*
+import com.example.brzodolokacije.Fragments.ExploreFragment
+import com.example.brzodolokacije.Fragments.HomeFragment
+import com.example.brzodolokacije.Fragments.ProfileFragment
 import com.example.brzodolokacije.Managers.SessionManager
 import com.example.brzodolokacije.Models.DefaultResponse
 import com.example.brzodolokacije.Posts.PrivremeneSlikeZaFeed
-import com.example.brzodolokacije.databinding.ActivityMainBinding
 import com.example.brzodolokacije.R
+import com.example.brzodolokacije.databinding.ActivityMainBinding
 import io.ak1.BubbleTabBar
 import io.ak1.OnBubbleClickListener
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,11 +33,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         PrivremeneSlikeZaFeed.addPhotos()
+
+//        val intent = getIntent()
+//        val provera = intent.getStringExtra("backPressed");
+//        if (provera != null)
+//        {
+//            replaceFragment(ProfileFragment())
+//            val bubbleTab = findViewById<BubbleTabBar>(R.id.bubbleTabBar);
+//            bubbleTab.setSelected(2, false)
+//        }
+//        else
+//        {
+//            replaceFragment(HomeFragment())
+//        }
+
         replaceFragment(HomeFragment())
 
         bubbleTabBar.addBubbleListener(object : OnBubbleClickListener{
             override fun onBubbleClick(id: Int) {
-
                 when(id){
                     R.id.explore -> replaceFragment(ExploreFragment())
                     R.id.home -> replaceFragment(HomeFragment())
