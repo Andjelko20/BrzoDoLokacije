c947b22ffc54fa45bef8d24f5ed1f2dc15eed1c1
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-18 23:08:55 +0100

    #45 Uspesan upload fotografije (odabir iz galerije)

--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/ActivityEditProfile.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/ActivityEditProfile.kt
@@ -1,13 +1,23 @@
 package com.example.brzodolokacije.Activities
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Base64
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.brzodolokacije.API.Api
 import com.example.brzodolokacije.Client.Client
 import com.example.brzodolokacije.Constants.Constants
@@ -25,6 +35,9 @@ import retrofit2.Response
 
 class ActivityEditProfile : AppCompatActivity() {
 
+    var pickedPhoto : Uri? = null
+    var pickedBitMap : Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -38,6 +51,49 @@ class ActivityEditProfile : AppCompatActivity() {
         }
 
         fillData()
+    }
+
+    fun pickPhoto(view: View){
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1)
+        } else {
+            val galeriIntext = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galeriIntext,2)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray)
+    {
+        if (requestCode == 1) {
+            if (grantResults.size > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galeriIntext = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntext,2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            pickedPhoto = data.data
+            if (pickedPhoto != null) {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    val source = ImageDecoder.createSource(this.contentResolver,pickedPhoto!!)
+                    pickedBitMap = ImageDecoder.decodeBitmap(source)
+                    editProfilePicture.setImageBitmap(pickedBitMap)
+                }
+                else {
+                    pickedBitMap = MediaStore.Images.Media.getBitmap(this.contentResolver,pickedPhoto)
+                    editProfilePicture.setImageBitmap(pickedBitMap)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun fillData()
