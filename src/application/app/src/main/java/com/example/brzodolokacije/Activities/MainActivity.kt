8844fd548241322cf5963604a14a0a5f4166546a
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-18 02:11:57 +0100

    #40 Uskladjena promena fragmenta i selektovanog itema u nav baru aplikacije nakon sto se preko back dugmeta sa edit ptofile vratimo na profile

--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/MainActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.brzodolokacije.Activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.brzodolokacije.API.Api
 import com.example.brzodolokacije.Client.Client
@@ -34,22 +36,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         PrivremeneSlikeZaFeed.addPhotos()
 
-//        val intent = getIntent()
-//        val provera = intent.getStringExtra("backPressed");
-//        if (provera != null)
-//        {
-//            replaceFragment(ProfileFragment())
-//            bubbleTabBar.setSelected(2, false)
-//        }
-//        else
-//        {
-//            replaceFragment(HomeFragment())
-//            bubbleTabBar.setSelected(0, false)
-//        }
+        val intent = getIntent()
+        val provera = intent.getStringExtra("backPressed");
+        if (provera != null)
+        {
+            replaceFragment(ProfileFragment())
+            bubbleTabBar.visibility = View.INVISIBLE;
+            bubbleTabBar2.visibility = View.VISIBLE;
+        }
+        else
+        {
+            replaceFragment(HomeFragment())
+            bubbleTabBar.visibility = View.VISIBLE;
+            bubbleTabBar2.visibility = View.INVISIBLE;
+        }
 
-        replaceFragment(HomeFragment())
+//        replaceFragment(HomeFragment())
 
         bubbleTabBar.addBubbleListener(object : OnBubbleClickListener{
+            override fun onBubbleClick(id: Int) {
+                when(id){
+                    R.id.explore -> replaceFragment(ExploreFragment())
+                    R.id.home -> replaceFragment(HomeFragment())
+                    R.id.profile -> replaceFragment(ProfileFragment())
+
+                    else -> {}
+                }
+
+            }
+        })
+
+        bubbleTabBar2.addBubbleListener(object : OnBubbleClickListener{
             override fun onBubbleClick(id: Int) {
                 when(id){
                     R.id.explore -> replaceFragment(ExploreFragment())
