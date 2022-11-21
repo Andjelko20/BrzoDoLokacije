ab693ae3f4831dd79da7ae3e74f97d45f44e27e8
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-21 01:38:27 +0100

    #59 Zavrsen izgled

---
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/ChangePasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.brzodolokacije.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
