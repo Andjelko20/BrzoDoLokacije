3d7b405d8f1694aabdedb0d9985a4c23c88276c6
kiiviiii <ivana.dragovic07@gmail.com>
2022-11-28 12:23:57 +0100

    #72 Dodata opcija Change Password u options menu + mala izmena izgleda i funkcionalnosti

--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/ChangePasswordActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/ChangePasswordActivity.kt
@@ -1,12 +1,28 @@
 package com.example.brzodolokacije.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.brzodolokacije.R
 
 class ChangePasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+
+        val cancelPasswordButton = findViewById<Button>(R.id.cancelPasswordButton)
+        cancelPasswordButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("backToProfile", "returnToProfile");
+            startActivity(intent)
+        }
+
+        val buttonBackResetPassword = findViewById<Button>(R.id.backButtonResetPassword)
+        buttonBackResetPassword.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("backToProfile", "returnToProfile");
+            startActivity(intent)
+        }
     }
 }
