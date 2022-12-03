0016df343ea35a936279b8a00fceb4994f37aebe
KatVir <94877518+KatVir@users.noreply.github.com>
2022-12-03 23:20:34 +0100

    .

---
+++ src/application/app/src/main/java/com/example/brzodolokacije/Posts/HomeFragmentState.kt
@@ -0,0 +1,59 @@
+package com.example.brzodolokacije.Posts
+
+import android.util.Log
+
+object HomeFragmentState {
+//    var visitUser : String = ""
+    private var feed : String = ""
+    private var issaved : Boolean = false
+    private var listFeed : MutableList<Photo?>? = null
+    private var page : Int = 1
+
+//    fun setVisit(owner : String)
+//    {
+//        visitUser=owner
+//    }
+//    fun getVisit() : String
+//    {
+//        return visitUser
+//    }
+    fun saveFeed(set : String)
+    {
+        feed=set
+    }
+    fun retreiveFeed() : String
+    {
+        return feed
+    }
+
+    fun shouldSave(yes : Boolean)
+    {
+        issaved = yes
+    }
+
+    fun isSaved() : Boolean
+    {
+        return issaved
+    }
+
+    fun list(feed : MutableList<Photo?>?)
+    {
+        listFeed = feed
+//        Log.d("feed", listFeed.toString())
+    }
+
+    fun getList() : MutableList<Photo?>?
+    {
+        return listFeed
+    }
+
+    fun page(newPage : Int)
+    {
+        page = newPage
+    }
+
+    fun savedPage() : Int
+    {
+        return page
+    }
+}
