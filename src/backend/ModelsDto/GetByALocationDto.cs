92f8bf2979b480f91523f49127f9345e9c14f691
dragan.miljkovic <dragan.miljkovic29@gmail.com>
2022-12-04 21:25:53 +0100

    dodat prikaz objava po lokaciji i po useru na mapi i kao lista #89

---
+++ src/backend/ModelsDto/GetByALocationDto.cs
@@ -0,0 +1,7 @@
+namespace backend.ModelsDto;
+
+public class GetByALocationDto
+{
+    public string Location { get; set; }
+    public string Filter { get; set; } = string.Empty;
+}
---
+++ src/backend/ModelsDto/PostOnMapDto.cs
@@ -0,0 +1,8 @@
+namespace backend.ModelsDto;
+
+public class PostOnMapDto
+{
+    public int Id { get; set; }
+    public string Longitude { get; set; }
+    public string Latitude { get; set; }
+}
