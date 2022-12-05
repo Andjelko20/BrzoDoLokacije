a5ed047ebe00f47dc1a894d9f65725d0711571fb
kiiviiii <ivana.dragovic07@gmail.com>
2022-12-05 16:21:48 +0100

    #92 Uspeno preuzeti podaci o objavi (get request)

--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/ShowPostActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/ShowPostActivity.kt
@@ -6,10 +6,16 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import com.example.brzodolokacije.API.Api
+import com.example.brzodolokacije.Client.Client
 import com.example.brzodolokacije.Fragments2.HomeFragment
 import com.example.brzodolokacije.Fragments2.ProfileFragment
+import com.example.brzodolokacije.Models.DefaultResponse
 import com.example.brzodolokacije.R
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ShowPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +23,41 @@ class ShowPostActivity : AppCompatActivity() {
         setContentView(R.layout.activity_show_post)
 
         val intent = getIntent()
-        val postId = intent.getStringExtra("showPost");
-//        Log.d("clicked id", postId.toString())
+        val postIdStr = intent.getStringExtra("showPost");
+//        Log.d("clicked id", postIdStr.toString())
+
+        val postId = postIdStr?.toInt()
 
         val backButtonPostDetails = findViewById<Button>(R.id.backButtonPostDetails)
         backButtonPostDetails.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("backToProfile", "returnToProfile");
             startActivity(intent)
+        }
+
+        val retrofit = Client(this).buildService(Api::class.java)
+        if (postId != null) {
+            retrofit.getPostData(postId).enqueue(object : Callback<DefaultResponse>{
+                override fun onResponse(
+                    call: Call<DefaultResponse>,
+                    response: Response<DefaultResponse>
+                ) {
+                    if(response.body()?.error.toString() == "false")
+                    {
+                        val json = response.body()?.message.toString()
+                        Log.d("data", json)
+                    }
+                    else
+                    {
+                        Log.d("error", response.body()?.error.toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
+                    Log.d("failed", "")
+                }
+
+            })
         }
     }
 }
