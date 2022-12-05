837cea4dff099e4f8731bb6c50ee394c2ed471a9
KatVir <94877518+KatVir@users.noreply.github.com>
2022-12-06 00:06:47 +0100

    #96 direct message izgled

--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/ChatActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/ChatActivity.kt
@@ -2,11 +2,25 @@ package com.example.brzodolokacije.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.brzodolokacije.Fragments2.DirectMessageFragment
 import com.example.brzodolokacije.R
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        replaceFragment(DirectMessageFragment())
+
+        
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.chatSectionFrameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
