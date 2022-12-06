5c0de49d272c3f57b367c0783e0a921aa79f713a
dragan.miljkovic <dragan.miljkovic29@gmail.com>
2022-12-06 11:34:47 +0100

    Update Message.cs

--- src/backend/Models/Message.cs
+++ src/backend/Models/Message.cs
@@ -6,6 +6,7 @@ public class Message
 {
     public int Id { get; set; }
     public string Content { get; set; }
+    public long Date { get; set; } = DateTime.Now.Ticks;
     [JsonIgnore]
     public User Sender { get; set; }
     public int SenderId { get; set; }
