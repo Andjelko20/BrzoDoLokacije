8605f60c3d1e023af8b3f5732f0f54a34632f721
dragan.miljkovic <dragan.miljkovic29@gmail.com>
2022-12-06 15:00:37 +0100

    Update DirectMessageFragment.kt

--- src/application/app/src/main/java/com/example/brzodolokacije/Fragments2/DirectMessageFragment.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Fragments2/DirectMessageFragment.kt
@@ -99,6 +99,7 @@ class DirectMessageFragment : Fragment() {
                 val message = sendMessageText.text.toString().trim()
                 val sender = sessionManager?.fetchUsername()
                 val receiver = user
+                signalRListener.sendMessage(sender,receiver,message)
             }
         }
     }
