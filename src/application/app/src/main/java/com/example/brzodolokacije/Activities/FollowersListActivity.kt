c7ab4db5bf58ab1d89c9dbb21fde2b2163915ea3
kiiviiii <ivana.dragovic07@gmail.com>
2022-12-11 20:03:43 +0100

    #99 izmene

--- src/application/app/src/main/java/com/example/brzodolokacije/Activities/FollowersListActivity.kt
+++ src/application/app/src/main/java/com/example/brzodolokacije/Activities/FollowersListActivity.kt
@@ -1,12 +1,21 @@
 package com.example.brzodolokacije.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.brzodolokacije.R
 
 class FollowersListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_followers_list)
+
+        val backButtonFollowersSection = findViewById<Button>(R.id.backButtonFollowersSection);
+        backButtonFollowersSection.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("backToProfile", "returnToProfile");
+            startActivity(intent)
+        }
     }
 }
