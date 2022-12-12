893a2baca337fc4de6d05c474cc62a3e5bcf3870
KatVir <94877518+KatVir@users.noreply.github.com>
2022-12-12 23:56:51 +0100

    .

--- src/ai-microservice/main.py
+++ src/ai-microservice/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 import cv2
+import numpy as np
 
 app = FastAPI()
 
@@ -10,7 +11,10 @@ async def root():
 
 @app.post("/avatar")
 async def avatar(picture : UploadFile = File(...)):
+    contents = await picture.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     face_cascade = cv2.CascadeClassifier('frontal_face.xml')
-    face_rects = face_cascade.detectMultiScale((picture,scaleFactor=1.2, minNeighbors=10)
-    return {"numOfFaces":face_rects.shape[0]}
+    face_rects = face_cascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=3)
+    return {"numOfFaces":img}
     #return {"status":False} # kad nije dobar format slike
